464aa33c42710e69e93d39831c099b830a929f95
Александр Пермяков <nathan920@yandex.ru>
2022-10-21 21:48:00 +0300

    Django HW2

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
+import csv
 
 
 def index(request):
@@ -7,11 +9,15 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
-    context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
-    }
-    return render(request, 'stations/index.html', context)
+    with open('pagination/data-398-2018-08-30.csv', newline='\n', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        CONTENT = [name for name in reader]
+        param_page = int(request.GET.get('page', 1))
+        content = Paginator(CONTENT, 10)
+        stations = content.get_page(param_page)
+        page = content.get_page(param_page)
+        context = {
+            'bus_stations': stations,
+            'page': page,
+        }
+        return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,5 @@
+from http.client import HTTPResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 DATA = {
@@ -16,15 +18,12 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipes(request, dish):
+    servings = request.GET.get('servings')
+    context = {'recipe': DATA[dish]}
+    for ing in context['recipe']:
+        context['recipe'][ing] *= int(servings)
+    return render(request, 'calculator/index.html', context=context)
