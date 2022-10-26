2ccabc14842218cf6055d50a9d8d5fa54e11a5f9
Александр Пермяков <nathan920@yandex.ru>
2022-10-26 11:23:36 +0300

    HW2 Done

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,7 +1,7 @@
 import csv
-
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.template.defaultfilters import slugify
 
 
 class Command(BaseCommand):
@@ -9,9 +9,11 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
+        with open('work_with_database/phones_info.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            Phone.objects.create(
+                            name=phone['name'], image=phone['image'],
+                            price=phone['price'], release_date=phone['release_date'],
+                            lte_exists=phone['lte_exists'], slug=slugify(phone['name'])).save()
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=70)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField(max_length=400)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
