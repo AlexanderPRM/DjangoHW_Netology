2bdc6b283f5fd83fbf61a4b93ecdf8bd4acb42bf
Александр Пермяков <nathan920@yandex.ru>
2022-11-17 16:50:05 +0300

    CRUD HW Done

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,27 @@
 from rest_framework import serializers
+from .models import Stock, StockProduct, Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = '__all__'
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = '__all__'
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -22,10 +29,11 @@ class StockSerializer(serializers.ModelSerializer):
 
         # создаем склад по его параметрам
         stock = super().create(validated_data)
-
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
 
         return stock
 
@@ -36,8 +44,12 @@ class StockSerializer(serializers.ModelSerializer):
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        for position in positions:
+            obj, created = StockProduct.objects.update_or_create(
+                                                stock=stock,
+                                                product=position['product'],
+                                                defaults={
+                                                    'quantity': position['quantity'],
+                                                    'price': position['price']})
 
         return stock
